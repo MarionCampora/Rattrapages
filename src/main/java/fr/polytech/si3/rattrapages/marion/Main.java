23cea83094110c76cb173630d2df93d2f4f82bff
Marion Campora <marion.campora@etu.unice.fr>
2017-08-27 19:54:55 +0200

    First commit #3

---
+++ src/main/java/fr/polytech/si3/rattrapages/marion/Main.java
@@ -0,0 +1,10 @@
+package fr.polytech.si3.rattrapages.marion;
+
+/**
+ * Created by Marion on 27/08/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
