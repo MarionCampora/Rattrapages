4b4f9e727126966f4259acfb2c12ca24048dd653
Marion Campora <marion.campora@etu.unice.fr>
2017-08-28 22:32:38 +0200

    Creation of House #id 6

---
+++ src/main/java/fr/polytech/si3/rattrapages/marion/building/Building.java
@@ -0,0 +1,16 @@
+package fr.polytech.si3.rattrapages.marion.building;
+
+/**
+ * @author Marion
+ */
+public class Building {
+    protected int id;
+
+    public Building(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/main/java/fr/polytech/si3/rattrapages/marion/building/House.java
@@ -0,0 +1,10 @@
+package fr.polytech.si3.rattrapages.marion.building;
+
+/**
+ * @author Marion
+ */
+public class House extends Building {
+    public House(int id) {
+        super(id);
+    }
+}
