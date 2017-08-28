4ab07ad40d7937e3f6e234cab7bb0fa86ddfad54
Marion Campora <marion.campora@etu.unice.fr>
2017-08-28 22:57:18 +0200

    Creation of PowerSource #id 5

--- src/main/java/fr/polytech/si3/rattrapages/marion/Main.java
+++ src/main/java/fr/polytech/si3/rattrapages/marion/Main.java
@@ -1,10 +1,39 @@
 package fr.polytech.si3.rattrapages.marion;
 
+import fr.polytech.si3.rattrapages.marion.graph.WeightedEdge;
+import fr.polytech.si3.rattrapages.marion.graph.WeightedUnDiGraph;
+import fr.polytech.si3.rattrapages.marion.graph.algorithm.KruskalAvecSources;
+import fr.polytech.si3.rattrapages.marion.heap.EmptyHeapException;
+import fr.polytech.si3.rattrapages.marion.heap.FullHeapException;
+
+import java.util.Set;
+
 /**
- * Created by Marion on 27/08/2017.
+ * @author Marion
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World !");
+    public static void main(String args[]) throws FullHeapException, EmptyHeapException {
+        KruskalAvecSources k = new KruskalAvecSources();
+        WeightedUnDiGraph G = new WeightedUnDiGraph(9);
+        G.addEdge(0,2,4);
+        G.addEdge(0,5,8);
+        G.addEdge(1,4,2);
+        G.addEdge(1,5,7);
+        G.addEdge(1,7,6);
+        G.addEdge(2,4,8);
+        G.addEdge(2,5,11);
+        G.addEdge(3,4,4);
+        G.addEdge(3,6,10);
+        G.addEdge(3,7,2);
+        G.addEdge(3,8,14);
+        G.addEdge(4,8,7);
+        G.addEdge(5,7,1);
+        G.addEdge(6,8,9);
+
+        Set<WeightedEdge> mst = k.mst(G);
+
+        for ( WeightedEdge e : mst )
+            System.out.print(e + " ");
+        System.out.println();
     }
 }
---
+++ src/main/java/fr/polytech/si3/rattrapages/marion/building/PowerSource.java
@@ -0,0 +1,10 @@
+package fr.polytech.si3.rattrapages.marion.building;
+
+/**
+ * @author Marion
+ */
+public class PowerSource extends Building {
+    public PowerSource(int id) {
+        super(id);
+    }
+}
