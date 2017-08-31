1a8e117a8c4f8ec47e362d9fb486576e20365fa3
Marion Campora <marion.campora@etu.unice.fr>
2017-08-31 15:11:17 +0200

    Kruskal with 2 sources #id 17

--- src/main/java/fr/polytech/si3/rattrapages/marion/graph/algorithm/KruskalAvecSources.java
+++ src/main/java/fr/polytech/si3/rattrapages/marion/graph/algorithm/KruskalAvecSources.java
@@ -41,44 +41,57 @@ public class KruskalAvecSources extends Algorithm{
         return find(fathers[x]);
     }
 
-    private void union(int root1, int root2){
+    private boolean union(int root1, int root2){
         int cpt = 0;
-        int powerSource = 0;
+        int powerSource = -1;
+        boolean isRoot1PowerSource = false;
+        boolean isRoot2PowerSource = false;
         for (int i = 0; i < edge.getPowerSourceList().size(); i++) {
             if (root1 == edge.getPowerSourceList().get(i).getId()){
                 cpt = 1;
                 powerSource = root1;
+                isRoot1PowerSource = true;
             }
             if (root2 == edge.getPowerSourceList().get(i).getId()){
                 cpt = 2;
                 powerSource = root2;
+                isRoot2PowerSource = true;
             }
         }
-        if (cpt == 0) {
-            if (fathers[root1] >= fathers[root2]) {
-                fathers[root2] = root1;
-                if (fathers[root1] == -1) {
-                    fathers[root1] = root1;
-                }
-            } else {
-                fathers[root1] = root2;
-                if (fathers[root2] == -1) {
-                    fathers[root2] = root2;
+            if (cpt == 0) {
+                if (fathers[root1] >= fathers[root2]) {
+                    fathers[root2] = root1;
+                    if (fathers[root1] == -1) {
+                        fathers[root1] = root1;
+                    }
+                } else {
+                    fathers[root1] = root2;
+                    if (fathers[root2] == -1) {
+                        fathers[root2] = root2;
+                    }
                 }
             }
+            if (cpt == 1) {
+                fathers[root2] = powerSource;
+            }
+            if (cpt == 2) {
+                fathers[root1] = powerSource;
+
         }
-        if (cpt == 1){
-            fathers[root2] = powerSource;
+
+        if (isRoot1PowerSource == true && isRoot2PowerSource == true){
+            return false;
         }
-        if (cpt == 2){
-            fathers[root1] = powerSource;
+        else {
+            return true;
         }
     }
-    
+
     private void createdTheGraph (List<List<Integer>> sortLinkedList, List<List<Integer>> kruskalList, int j, int[] fathers){
         if (find(fathers[sortLinkedList.get(j).get(0)]) != find(fathers[sortLinkedList.get(j).get(1)]) || find(fathers[sortLinkedList.get(j).get(0)]) == -1 || find(fathers[sortLinkedList.get(j).get(1)]) == -1){
-            union(fathers[sortLinkedList.get(j).get(0)], fathers[sortLinkedList.get(j).get(1)]);
-            kruskalList.add(sortLinkedList.get(j));
+            if(union(fathers[sortLinkedList.get(j).get(0)], fathers[sortLinkedList.get(j).get(1)])) {
+                    kruskalList.add(sortLinkedList.get(j));
+                }
             mstEdges++;
         }
     }
