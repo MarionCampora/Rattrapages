f2bd4551e33192e6feca312668984ee54d67592a
Marion Campora <marion.campora@etu.unice.fr>
2017-08-31 14:26:16 +0200

    Kruskal with 2 sources beginning fix #id 17

--- src/main/java/fr/polytech/si3/rattrapages/marion/Main.java
+++ src/main/java/fr/polytech/si3/rattrapages/marion/Main.java
@@ -1,7 +1,5 @@
 package fr.polytech.si3.rattrapages.marion;
 
-import fr.polytech.si3.rattrapages.marion.heap.EmptyHeapException;
-import fr.polytech.si3.rattrapages.marion.heap.FullHeapException;
 import fr.polytech.si3.rattrapages.marion.ihm.Screen;
 
 
@@ -9,7 +7,7 @@ import fr.polytech.si3.rattrapages.marion.ihm.Screen;
  * @author Marion
  */
 public class Main {
-    public static void main(String args[]) throws FullHeapException, EmptyHeapException {
+    public static void main(String args[]){
         Screen screen = new Screen();
         screen.display();
     }
